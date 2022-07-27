086499582a3bab67649d5f5a529a6e7ab5d7d764
ekaterina <katerina_w@icloud.com>
2022-07-27 23:38:10 +0300

    Создали два массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string[] OneArray = new string[6] { "morning", "fox", "life", "sea", "glory", "cool"}; 
+string[] TwoArray = new string[OneArray.Length]; 
